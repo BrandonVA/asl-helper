57f004975c7e581c89d40f6331e2184681f1500b
BrandonVA <codebva@gmail.com>
2022-08-02 13:43:16 -0700

    Updated nav elements

--- client/src/components/NavHeading/NavHeading.js
+++ client/src/components/NavHeading/NavHeading.js
@@ -9,8 +9,7 @@ function NavHeading() {
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
