160e186a2134ea7017381be6d85cda8f434664a4
BrandonVA <codebva@gmail.com>
2022-08-02 13:42:37 -0700

    Added theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+      minBreakpoint="xxs"
+    >
+      <div className="App">
+        <Home />
+      </div>
+    </ThemeProvider>
   );
 }
 
