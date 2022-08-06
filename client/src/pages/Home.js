e0256b04b499ec46464874543272849c15658d5c
BrandonVA <codebva@gmail.com>
2022-08-05 21:29:56 -0700

    Created handle input funtion for the search bar.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import IconContainer from "../components/IconContainer/IconContainer";
 import InputContainer from "../components/InputContainer/InputContainer";
 import NavHeading from "../components/NavHeading/NavHeading";
 
 function Home() {
+  const [inputData, setInputData] = useState();
+
+  const handleInputChange = (e) => {
+    let inputText = e.target.value.toLowerCase();
+
+    setInputData(inputText);
+    console.log(inputText);
+  };
+  console.log(inputData);
   return (
     <>
       <NavHeading />
-      <InputContainer />
+      <InputContainer handleInputChange={handleInputChange} />
       <IconContainer />
     </>
   );
